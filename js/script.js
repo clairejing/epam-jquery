6c51f1c4f064985b456202e2705a33cd6a8a00d1
Clairejing <wenjing.liu09@foxmail.com>
2016-02-25 11:25:22 +0800

    finish practice index.html

--- js/script.js
+++ js/script.js
@@ -8,38 +8,41 @@ $("#prepend button").click(function() {
 });
 
 $("#before button").click(function() {
-	
+	$("#before p").before("Hi");
 });
 
 $("#css1 button").click(function() {
-	
+  $("#css1 p").css("font-size","20px");
 });
 
 $("#css2 button").click(function() {
-	
+  $("#css2 p").css({"color":"red","font-size":"20px"});
 });
 
 $("#attr1 button").click(function() {
-	
+  alert($("#attr1 p a").attr("href"));
 });
 
 $("#attr2 button").click(function() {
 
+  $("#attr2 p a").attr("href","http://www.baidu.com");
 });
 
 $("#class1 button").click(function() {
-	
+  $("#class1 p").addClass("bg-primary");
 });
 
 $("#class2 button").click(function() {
-
+  alert($("class2 p").hasClass("lead"));
 });
 
 $("#form1 button").click(function() {
-
+  console.log($("#form1 #exampleInputEmail1").val());
 });
 
 $("#form2 button").click(function() {
+    debugger
+  $("#exampleInputName2").val("Claire");
 
 });
 
